033992ec74f98cda54a7be85d6a0dea711b0314c
Marat Colesnic <marat.colesnic@gmail.com>
2025-04-28 11:59:07 +0200

    Fix deploy.yml: deploy dist folder correctly

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import * as path from 'path';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: '/Greenhouse-Frontend/', // github pages deplyment
+  base: mode === 'development' ? '/' : '/Greenhouse-Frontend/', // ✅ Local = '/', GitHub Pages = '/Greenhouse-Frontend/'
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
@@ -14,4 +14,4 @@ export default defineConfig({
     port: 5173,
     open: true,
   },
-});
+}));
