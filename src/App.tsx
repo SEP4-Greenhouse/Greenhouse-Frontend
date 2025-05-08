a03b456258a3042ab490bd5ddf02a30e48a3f5b6
Marat Colesnic <marat.colesnic@gmail.com>
2025-05-08 15:48:22 +0300

    CD test log from frontend-changes-marat

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@
 import { Outlet } from "react-router-dom";
 import { AuthProvider } from "./features/auth/AuthContext"; // ✅ Ensure path is correct
 
+console.log("Testing CD from frontend-changes-marat branch");
+
+
 
 const App = () => {
   return (
