97db305a14f82b97822508168ef0fc2a62a53619
Marat Colesnic <marat.colesnic@gmail.com>
2025-05-08 18:10:34 +0300

    Test CD with updated Docker token

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,6 @@
 import { Outlet } from "react-router-dom";
 import { AuthProvider } from "./features/auth/AuthContext"; // ✅ Ensure path is correct
 
-console.log("Token fixed");
-
-
-
-
 const App = () => {
   return (
     <AuthProvider>
