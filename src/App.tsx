cf4232a618032090731f0ebb1c4ef4bc5d1646ab
Marat Colesnic <marat.colesnic@gmail.com>
2025-05-08 16:08:54 +0300

    Test CD with updated Docker token

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@
 import { Outlet } from "react-router-dom";
 import { AuthProvider } from "./features/auth/AuthContext"; // ✅ Ensure path is correct
 
-console.log("Testing CD from frontend-changes-marat branch");
+console.log("Token fixed");
+
 
 
 
