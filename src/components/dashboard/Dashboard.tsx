c285a9964c18b732778fa41d8010d702f3ac117c
patrikklimko <patrik.klimko123@gmail.com>
2025-05-27 13:25:12 +0200

    final changes
    removing react

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import "./dashboard.css";
 import StatCard from "./StatCard";
 import AlertsPanel from "./AlertsPanel";
